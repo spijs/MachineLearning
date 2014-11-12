b4a3016fe90b4579a874dcf4d28e840b0686ff3d
Bram Geelen <bramgeelen@gmail.com>
2014-11-12 16:43:08 +0100

    expand Walk

--- src/model/Walk.java
+++ src/model/Walk.java
@@ -4,13 +4,60 @@ import java.util.ArrayList;
 
 public class Walk {
 
-	final ArrayList<Point> points;
+	final String name;
+	final private ArrayList<Point> points;
 
-	public Walk() {
+	public Walk(String name) {
+		this.name = name;
 		this.points = new ArrayList<>();
 	}
 
-	public Walk(ArrayList<Point> points) {
+	public Walk(String name, ArrayList<Point> points) {
+		this.name = name;
 		this.points = points;
+	}
+
+	public Point getPoint(int i) {
+		return points.get(i);
+	}
+
+	public int getSize() {
+		return points.size();
+	}
+
+	public void addPoint(Point p) {
+		points.add(p);
+	}
+
+	public ArrayList<Double> getTValues() {
+		ArrayList<Double> returnList = new ArrayList<>();
+		for (Point point : points) {
+			returnList.add(point.t);
+		}
+		return returnList;
+	}
+
+	public ArrayList<Double> getXValues() {
+		ArrayList<Double> returnList = new ArrayList<>();
+		for (Point point : points) {
+			returnList.add(point.x);
+		}
+		return returnList;
+	}
+
+	public ArrayList<Double> getYValues() {
+		ArrayList<Double> returnList = new ArrayList<>();
+		for (Point point : points) {
+			returnList.add(point.y);
+		}
+		return returnList;
+	}
+
+	public ArrayList<Double> getZValues() {
+		ArrayList<Double> returnList = new ArrayList<>();
+		for (Point point : points) {
+			returnList.add(point.z);
+		}
+		return returnList;
 	}
 }
