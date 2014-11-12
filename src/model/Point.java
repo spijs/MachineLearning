fef4d3dcb789f344ceed6f0ff1a3cc6ce31704a7
Bram Geelen <bramgeelen@gmail.com>
2014-11-12 16:36:14 +0100

    added model package

---
+++ src/model/Point.java
@@ -0,0 +1,21 @@
+package model;
+
+/**
+ *
+ * @author bram
+ */
+public class Point {
+
+	public double t;
+	public double x;
+	public double y;
+	public double z;
+
+	public Point(double t, double x, double y, double z) {
+		this.t = t;
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+
+}
---
+++ src/model/Walk.java
@@ -0,0 +1,16 @@
+package model;
+
+import java.util.ArrayList;
+
+public class Walk {
+
+	final ArrayList<Point> points;
+
+	public Walk() {
+		this.points = new ArrayList<>();
+	}
+
+	public Walk(ArrayList<Point> points) {
+		this.points = points;
+	}
+}
