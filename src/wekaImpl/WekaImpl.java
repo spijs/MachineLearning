2d7ccf3a0350b7f6276aec1dd07f201bca36cb91
Bram Geelen <bramgeelen@gmail.com>
2014-11-12 19:05:42 +0100

    wekaImpl done for testing
    usage should be moved from prepareClassifier() to useClassifier()

--- src/wekaImpl/WekaImpl.java
+++ src/wekaImpl/WekaImpl.java
@@ -1,15 +1,20 @@
 package wekaImpl;
 
+import extractor.FeatureExtractor;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import model.Dataset;
 import model.Feature;
-import model.FeatureExtractor;
 import model.Walk;
+import weka.classifiers.Classifier;
+import weka.classifiers.Evaluation;
 import weka.core.Attribute;
 import weka.core.FastVector;
+import weka.core.Instance;
 import weka.core.Instances;
 
 /**
@@ -18,11 +23,12 @@ import weka.core.Instances;
  * @author bram
  */
 public class WekaImpl {
+
 	private final Dataset dataset;
 	private Map<String, Attribute> attributes;
 
 	private FastVector wekaAttributes;
-	private Instances trainingSet;
+	private Instances wekaTrainingSet;
 
 	public WekaImpl(Dataset dataset) {
 		this.dataset = dataset;
@@ -30,8 +36,8 @@ public class WekaImpl {
 
 	public void run() {
 		makeHeader();
-//		makeInstances();
-
+		makeInstances();
+		prepareClassifier();
 	}
 
 	public void makeHeader() {
@@ -67,4 +73,48 @@ public class WekaImpl {
 		}
 	}
 
+	public void makeInstances() {
+		wekaTrainingSet = new Instances("TrainingSet", wekaAttributes, dataset.numWalks());
+		Map<String, Feature.Type> extractedFeatures = FeatureExtractor.getFeatures();
+
+		for (int i = 0; i < dataset.numWalks(); i++) {
+			Walk walk = dataset.getWalk(i);
+			Map<String, Feature> features = dataset.getFeatures(walk);
+
+			Instance instance = new Instance(extractedFeatures.size());
+
+			for (int j = 0; j < wekaAttributes.size(); j++) {
+				Attribute attribute = (Attribute) wekaAttributes.elementAt(j);
+				String name = attribute.name();
+				if (features.get(name).type == Feature.Type.DOUBLE) {
+					instance.setValue(attribute, (double) features.get(attribute.name()).value);
+				} else {
+					instance.setValue(attribute, (String) features.get(attribute.name()).value);
+				}
+			}
+
+			wekaTrainingSet.add(instance);
+		}
+	}
+
+	public void prepareClassifier(Classifier classifier) {
+		try {
+			classifier.buildClassifier(wekaTrainingSet);
+
+			// everything following this should be moved to "useClassifier"
+			Evaluation evaluation = new Evaluation(wekaTrainingSet);
+			evaluation.evaluateModel(classifier, wekaTrainingSet);
+
+			// Print the result à la Weka explorer:
+			String strSummary = evaluation.toSummaryString();
+			System.out.println(strSummary);
+
+			// Get the confusion matrix
+			double[][] cmMatrix = evaluation.confusionMatrix();
+
+
+		} catch (Exception ex) {
+			Logger.getLogger(WekaImpl.class.getName()).log(Level.SEVERE, null, ex);
+		}
+	}
 }
