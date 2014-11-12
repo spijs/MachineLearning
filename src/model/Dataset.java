8d3e755fec612f9623dac627f1c7ed5d1fb664f3
Bram Geelen <bramgeelen@gmail.com>
2014-11-12 17:07:42 +0100

    add Dataset

---
+++ src/model/Dataset.java
@@ -0,0 +1,16 @@
+
+package model;
+
+import java.util.List;
+
+/**
+ *
+ * @author bram
+ */
+public class Dataset {
+	final List<Walk> walks;
+
+	public Dataset(List<Walk> walks) {
+		this.walks = walks;
+	}
+}
