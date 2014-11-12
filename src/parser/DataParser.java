85b2f299ab91d02718681db59f0f2e9b9b368a0e
spijs <thijsdieltjens@gmail.com>
2014-11-12 16:47:49 +0100

    haakje te veel

--- src/parser/DataParser.java
+++ src/parser/DataParser.java
@@ -35,7 +35,7 @@ public class DataParser {
 			while ((nextLine = reader.readNext()) != null) {
 				// nextLine[] is an array of values from the line
 				double time= Double.parseDouble(nextLine[0]);
-				double x = Double.parseDouble(nextLine[1]]);
+				double x = Double.parseDouble(nextLine[1]);
 				double y = Double.parseDouble(nextLine[2]);
 				double z = Double.parseDouble(nextLine[3]);
 				Point point = new Point(time, x,y,z);
