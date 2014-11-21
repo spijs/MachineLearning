ae02c1c9524a34de9c22235e0f717588c969ad4a
spijs <thijsdieltjens@gmail.com>
2014-11-21 14:21:57 +0100

    refactor featureextractor

--- src/extractor/FeatureExtractor.java
+++ src/extractor/FeatureExtractor.java
@@ -12,9 +12,11 @@ import model.Feature.Type;
 public class FeatureExtractor {
 
 	private Walk walk;
+	private Map<String, Feature> features;
 
 	public FeatureExtractor(Walk walk){
 		this.walk = walk;
+		features = new HashMap<String,Feature>();
 	}
 
 	public static Map<String, Feature.Type> getFeatures() {
@@ -30,145 +32,60 @@ public class FeatureExtractor {
 		returnMap.put("medianX",Feature.Type.DOUBLE);
 		returnMap.put("medianY",Feature.Type.DOUBLE);
 		returnMap.put("medianZ",Feature.Type.DOUBLE);
-		returnMap.put("FirstPercentileX",Feature.Type.DOUBLE);
-		returnMap.put("FirstPercentileY",Feature.Type.DOUBLE);
-		returnMap.put("FirstPercentileZ",Feature.Type.DOUBLE);
-		returnMap.put("ThirdPercentileX",Feature.Type.DOUBLE);
-		returnMap.put("ThirdPercentileY",Feature.Type.DOUBLE);
-		returnMap.put("ThirdPercentileZ",Feature.Type.DOUBLE);
+		returnMap.put("firstPercentileX",Feature.Type.DOUBLE);
+		returnMap.put("firstPercentileY",Feature.Type.DOUBLE);
+		returnMap.put("firstPercentileZ",Feature.Type.DOUBLE);
+		returnMap.put("thirdPercentileX",Feature.Type.DOUBLE);
+		returnMap.put("thirdPercentileY",Feature.Type.DOUBLE);
+		returnMap.put("thirdPercentileZ",Feature.Type.DOUBLE);
 		
 		return returnMap;
 	}
 
 	public Map<String,Feature> extractFeatures(){
-		// Mean and SD
-		Map<String,Feature> features = new HashMap<String,Feature>();
-		features.put("meanOfX",new Feature(getMeanX(),Feature.Type.DOUBLE));
-		features.put("meanOfY",new Feature(getMeanY(),Feature.Type.DOUBLE));
-		features.put("meanOfZ",new Feature(getMeanZ(),Feature.Type.DOUBLE));
-
-		features.put("standDevX", new Feature(getStandardDeviationX(),Feature.Type.DOUBLE)); 
-		features.put("standDevY", new Feature(getStandardDeviationY(),Feature.Type.DOUBLE));
-		features.put("standDevZ",new Feature(getStandardDeviationZ(),Feature.Type.DOUBLE));
-		
-		//Percentiles
-		features.put("MedianX", new Feature(getMedianX(),Feature.Type.DOUBLE)); 
-		features.put("MedianY", new Feature(getMedianY(),Feature.Type.DOUBLE));
-		features.put("MedianZ",new Feature(getMedianZ(),Feature.Type.DOUBLE));
-		
-		features.put("FirstPercentileX", new Feature(getFirstPercentileX(),Feature.Type.DOUBLE)); 
-		features.put("FirstPercentileY", new Feature(getFirstPercentileY(),Feature.Type.DOUBLE));
-		features.put("FirstPercentileZ", new Feature(getFirstPercentileZ(), Feature.Type.DOUBLE));
-		
-		features.put("ThirdPercentileX", new Feature(getThirdPercentileX(),Feature.Type.DOUBLE)); 
-		features.put("ThirdPercentileY", new Feature(getThirdPercentileY(),Feature.Type.DOUBLE));
-		features.put("ThirdPercentileZ", new Feature(getThirdPercentileZ(), Feature.Type.DOUBLE));
-		
+		extractMeans();
+		extractStandardDeviations();
+		extractMedians();
+		extractFirstPercentiles();
+		extractThirdPercentiles();		
 		return features;
+	}
+
+
+	private void extractStandardDeviations() {
+		features.put("standDevX",new Feature(getStandardDeviation(walk.getXValues()),Feature.Type.DOUBLE));
+		features.put("standDevY",new Feature(getStandardDeviation(walk.getYValues()),Feature.Type.DOUBLE));
+		features.put("standDevZ",new Feature(getStandardDeviation(walk.getZValues()),Feature.Type.DOUBLE));
+	}
+
+	private void extractMeans() {
+		features.put("meanOfX",new Feature(mean(walk.getXValues()),Feature.Type.DOUBLE));
+		features.put("meanOfY",new Feature(mean(walk.getYValues()),Feature.Type.DOUBLE));
+		features.put("meanOfZ",new Feature(mean(walk.getZValues()),Feature.Type.DOUBLE));
+	}
+
+	private void extractMedians(){
+		features.put("medianX",new Feature(getPercentile(walk.getXValues(),0.5),Feature.Type.DOUBLE));
+		features.put("medianY",new Feature(getPercentile(walk.getYValues(),0.5),Feature.Type.DOUBLE));
+		features.put("medianZ",new Feature(getPercentile(walk.getZValues(),0.5),Feature.Type.DOUBLE));
+	}
+	
+	
+	private void extractFirstPercentiles(){
+		features.put("firstPercentileX",new Feature(getPercentile(walk.getXValues(),0.25),Feature.Type.DOUBLE));
+		features.put("firstPercentileY",new Feature(getPercentile(walk.getYValues(),0.25),Feature.Type.DOUBLE));
+		features.put("firstPercentileZ",new Feature(getPercentile(walk.getZValues(),0.25),Feature.Type.DOUBLE));
+	}
+	
+	private void extractThirdPercentiles(){
+		features.put("thirdPercentileX",new Feature(getPercentile(walk.getXValues(),0.75),Feature.Type.DOUBLE));
+		features.put("thirdPercentileY",new Feature(getPercentile(walk.getYValues(),0.75),Feature.Type.DOUBLE));
+		features.put("thirdPercentileZ",new Feature(getPercentile(walk.getZValues(),0.75),Feature.Type.DOUBLE));
+	}
+	
+	
+	private void extractFFT(){
 		
-	}
-
-	/**
-	 * @param a
-	 * @param percentile : bvb 0.25 of 0.75
-	 * @return
-	 */
-	public double getPercentile(List<Double> a, double percentile){
-		 int middle = (int) (a.size()*percentile);
-	        if (a.size() % 2 == 1) {
-	            return a.get(middle);
-	        } else {
-	           return (a.get(middle-1) + a.get(middle)) / 2.0;
-	        }
-	}
-	private double getStandardDeviation(List<Double> values){
-
-		double sum = 0.0;
-		double mean = mean(values);
-
-		for (double i : values)
-			sum += Math.pow((i - mean), 2);
-		return Math.sqrt( sum / ( values.size() - 1 ) ); // sample
-
-	}
-
-	public double sum (List<Double> a){
-		if (a.size() > 0) {
-			double sum = 0;
-
-			for (double i : a) {
-				sum += i;
-			}
-			return sum;
-		}
-		return 0;
-	}
-
-	public double mean (List<Double> a){
-		double sum = sum(a);
-		double mean = 0;
-		mean = sum / (a.size() * 1.0);
-		return mean;
-	}
-
-	private double getStandardDeviationZ() {
-		return getStandardDeviation(walk.getZValues());
-	}
-
-	private double getStandardDeviationY() {
-		return getStandardDeviation(walk.getYValues());
-	}
-
-	private double getStandardDeviationX() {
-		return getStandardDeviation(walk.getXValues());
-	}
-
-	private double getMeanZ() {
-		return mean(walk.getZValues());
-	}
-
-	private double getMeanY() {
-		return mean(walk.getYValues());
-	}
-
-	private double getMeanX() {
-		return mean(walk.getXValues());
-	}
-
-	private double getMedianX(){
-		return getPercentile(walk.getXValues(),0.5);
-	}
-	
-	private double getMedianY(){
-		return getPercentile(walk.getYValues(),0.5);
-	}
-
-	private double getMedianZ(){
-		return getPercentile(walk.getZValues(),0.5);
-	}
-	
-	private double getFirstPercentileX(){
-		return getPercentile(walk.getXValues(),0.25);
-	}
-	
-	private double getFirstPercentileY(){
-		return getPercentile(walk.getYValues(),0.25);
-	}
-
-	private double getFirstPercentileZ(){
-		return getPercentile(walk.getZValues(),0.25);
-	}
-	
-	private double getThirdPercentileX(){
-		return getPercentile(walk.getXValues(),0.75);
-	}
-	
-	private double getThirdPercentileY(){
-		return getPercentile(walk.getYValues(),0.75);
-	}
-
-	private double getThirdPercentileZ(){
-		return getPercentile(walk.getZValues(),0.75);
 	}
 	
 	public double[] fftX() //TODO slechts de 5 grootste waarden teruggeven. + terug private zetten
@@ -186,5 +103,47 @@ public class FeatureExtractor {
 		    
 		 double [] result =  fft.fft(re, im);
 		 return result;
+	}
+	/**
+	 * @param a
+	 * @param percentile : bvb 0.25 of 0.75
+	 * @return
+	 */
+	public double getPercentile(List<Double> a, double percentile){
+		int middle = (int) (a.size()*percentile);
+		if (a.size() % 2 == 1) {
+			return a.get(middle);
+		} else {
+			return (a.get(middle-1) + a.get(middle)) / 2.0;
+		}
+	}
+	private double getStandardDeviation(List<Double> values){
+		
+		double sum = 0.0;
+		double mean = mean(values);
+		
+		for (double i : values)
+			sum += Math.pow((i - mean), 2);
+		return Math.sqrt( sum / ( values.size() - 1 ) ); // sample
+		
+	}
+	
+	public double sum (List<Double> a){
+		if (a.size() > 0) {
+			double sum = 0;
+			
+			for (double i : a) {
+				sum += i;
+			}
+			return sum;
+		}
+		return 0;
+	}
+	
+	public double mean (List<Double> a){
+		double sum = sum(a);
+		double mean = 0;
+		mean = sum / (a.size() * 1.0);
+		return mean;
 	}
 }
